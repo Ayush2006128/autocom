5a8082b3c11ba815f0ba4f32bd7479b0cc91a22e
Ayush Muley <ayush.muley1907@outlook.com>
2026-08-28 14:32:42 +0500

    refactor index screen to integrate accelerometer data and enhance fall detection display

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,18 @@
+import { useAccelerometer, useGyroscope } from "@/hooks/sensors";
+import { detectFall } from "@/lib/fall-detector";
 import { StyleSheet, Text, View } from "react-native";
-import { useGyroscope } from "../hooks/sensors";
 
 export default function Index() {
   const gyroscopeData = useGyroscope();
   const { x, y, z } = gyroscopeData;
+  const accelerometerData = useAccelerometer();
+  const { x: ax, y: ay, z: az } = accelerometerData;
+  const isFalling = detectFall(gyroscopeData, accelerometerData, Date.now());
   return (
     <View style={styles.container}>
-      <Text style={styles.xyz}>X: {x.toFixed(2)} rad/s</Text>
-      <Text style={styles.xyz}>Y: {y.toFixed(2)} rad/s</Text>
-      <Text style={styles.xyz}>Z: {z.toFixed(2)} rad/s</Text>
+      <Text style={styles.xyz}>Gyroscope: x={x.toFixed(2)}, y={y.toFixed(2)}, z={z.toFixed(2)}</Text>
+      <Text style={styles.xyz}>Accelerometer: x={ax.toFixed(2)}, y={ay.toFixed(2)}, z={az.toFixed(2)}</Text>
+      <Text style={styles.xyz}>{isFalling ? "Falling" : "Not Falling"}</Text>
     </View>
   );
 }

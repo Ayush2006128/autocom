83d8c715b433bf39dfd359b0e8c4165c586ec54a
Ayush Muley <ayush.muley1907@outlook.com>
2026-08-29 04:38:52 +0000

    use native ui

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import { useAccelerometer, useGyroscope } from "@/hooks/sensors";
 import { detectFall } from "@/lib/fall-detector";
-import { StyleSheet, Text, View } from "react-native";
+import { Host, Column, Text } from "@expo/ui";
+import { StyleSheet } from "react-native";
 
 export default function Index() {
   const gyroscopeData = useGyroscope();
@@ -9,21 +10,13 @@ export default function Index() {
   const { x: ax, y: ay, z: az } = accelerometerData;
   const isFalling = detectFall(gyroscopeData, accelerometerData, Date.now());
   return (
-    <View style={styles.container}>
-      <Text style={styles.xyz}>Gyroscope: x={x.toFixed(2)}, y={y.toFixed(2)}, z={z.toFixed(2)}</Text>
-      <Text style={styles.xyz}>Accelerometer: x={ax.toFixed(2)}, y={ay.toFixed(2)}, z={az.toFixed(2)}</Text>
-      <Text style={styles.xyz}>{isFalling ? "Falling" : "Not Falling"}</Text>
-    </View>
+    <Host style={{ flex: 1 }}>
+      <Column spacing={12} alignment="center">
+        <Text>Gyroscope: x={x.toFixed(2)}, y={y.toFixed(2)}, z={z.toFixed(2)}</Text>
+        <Text>Accelerometer: x={ax.toFixed(2)}, y={ay.toFixed(2)}, z={az.toFixed(2)}</Text>
+        <Text>{isFalling ? "Falling" : "Not Falling"}</Text>
+      </Column>
+    </Host>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  xyz: {
-    fontSize: 20,
-  }
-});
